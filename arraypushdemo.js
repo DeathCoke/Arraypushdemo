84e083c7a638749eaa727f30a5fa85d8ed72dd44
Axel Chan <33881844+DeathCoke@users.noreply.github.com>
2018-08-15 12:18:26 -1000

    Add files via upload

---
+++ arraypushdemo.js
@@ -0,0 +1,10 @@
+var okay = prompt("Give me your first name");
+var whatever = prompt("Give me your last name");
+
+var something = [];
+var anything = [];
+
+something.push(okay);
+anything.push(whatever);
+
+window.alert(okay + " " + something.length + " " + whatever + " " + anything.length);
